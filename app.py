3b0baed6f93cc1b1aa742239e00eee0dd47e54a0
Douglas <wdmdavison@icloud.com>
2024-04-02 21:43:24 -0500

    remove json import from app

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, redirect, url_for, session, request
 from dotenv import load_dotenv
 import requests
 from datetime import datetime
-import json
 
 app = Flask(__name__)
 
