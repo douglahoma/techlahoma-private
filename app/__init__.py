666c49b80a882f2a155760291df6be52596b97ef
Jonathan Boller <60049930+jorbian@users.noreply.github.com>
2024-04-09 16:47:11 -0500

    Update __init__.py
    Reverted to previous version because of bug I could not reproduce on Linux.

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,19 @@
 import os
 
 from .config import Config
-from .secretkey import SecretKey
-from flask import Flask
+from flask import Flask, session
 from flask_session import Session
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.secret_key = SecretKey()
+app.secret_key = os.getenv("SESSION_SECRET_KEY").encode()
 
 sess = Session()
 sess.init_app(app)
-
-# When testing locally just make sure you don't have the DOMAIN key defined
-# in the version of the .env file you're using and everyone should be okay.
-if (not os.getenv("DOMAIN")):
-    os.environ["REDIRECT_URI"] = "{}{}".format(
-        os.getenv("DEBUG_DOMAIN"),
-        os.getenv("REDIRECT_URI")
-    )
 
 from app import views
     
