67e37fc219670f63cf6ec2bfb9162794d6952fa2
jorbian <jonathan.boller@yahoo.co.uk>
2024-04-10 20:14:36 -0500

    Reverting to an old version while I figure out what happened with the server.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
+import os
+
 from .config import Config
-from .secretkey import SecretKey
-from flask import Flask
+from flask import Flask, session
 from flask_session import Session
 from dotenv import load_dotenv
 
@@ -9,12 +10,12 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.secret_key = SecretKey()
+app.secret_key = os.getenv("SESSION_SECRET_KEY").encode()
 
 sess = Session()
 sess.init_app(app)
 
-from . import views
+from app import views
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import os
 import requests
 
+from app import app, neoncrm
 from flask import render_template, session, request, redirect, url_for
 from dotenv import load_dotenv
 from datetime import datetime
